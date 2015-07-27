82a6d0a567c0be09689a2e73d3b263e9fd7c7bca
rack <373648449@qq.com>
2015-07-27 14:58:07 +0800

    增加adduser判断

--- fotoplace-autotest/src/main/java/com/fotoplace/http/user/AddUserServiceIT.java
+++ fotoplace-autotest/src/main/java/com/fotoplace/http/user/AddUserServiceIT.java
@@ -1,51 +1,52 @@
 package com.fotoplace.http.user;
- import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-import javax.xml.ws.Response;
+import java.util.Map;
 
 import com.alibaba.fastjson.JSON;
 import com.fotoplace.http.jsons.UserServiceJSON;
 import com.fotoplace.user.test.modl.UserRegister;
 import com.pajk.test.client.JsonRequestUtil;
 import com.pajk.test.client.ResultDO;
+import com.pajk.test.database.DBInfo;
+import com.pajk.test.database.DBOperate;
 
-import org.openqa.selenium.remote.HttpSessionId;
 import org.testng.Assert;
 import org.testng.annotations.*;
 
-import retrofit.client.Request;
 
  
 public class AddUserServiceIT{
 	
-	public static String serverIP="123.59.13.113:8180";
-	 /**
-     * @return
-     * @throws Exception
-     */
+	 public final String serverIP = DBInfo.getConn("serverIP");
+	 public final String conn =  DBInfo.getConn("conn");
+ 
+/*	 
+   @BeforeMethod
+   public void beforemethod() throws Exception{
+	   
+	    String paysql = "delete from user_profile where mobile='13774294435' ";
+	   DBOperate.execute(conn, paysql);
+	   Thread.sleep(2000);
+	    
+   }  
+	 */
     @Test(description = "新增用户信息-正常场景，创建人：郭强")
     public void addUser_normal() throws Exception{
-    	 
-    	String addUserJson =  UserServiceJSON.CreateUserServiceJson("12", "13774294435", "123456", "5623", "ios7.0", "ios", "3345", null);
+    	
+    	String addUserJson =  UserServiceJSON.CreateUserServiceJson("12", "13774294435", "123456", "5623", "ios7.0", "ios", "3345" );
     	
     	String URL ="http://" + serverIP + "/api/user/user_reg.php";
         
-       Cookie cook =  new Cookie("user",addUserJson);
-       cook.setMaxAge(60*60*24);
-       
-        
     	ResultDO  respone =  JsonRequestUtil.doPost(URL, addUserJson);
    
-    	
+    	//判断http返回结果
      	int statusCode =  respone.getStatusCode();
     	Assert.assertEquals(statusCode, 200);
     	
     	
     	String reg_json =  respone.getResultString();
-    	
     	UserRegister userinfo = new UserRegister();
     	userinfo = JSON.parseObject(reg_json, UserRegister.class);
+    	
     	
         Assert.assertEquals(userinfo.getOsType(), "ios");    
         Assert.assertEquals(userinfo.getMobile(), "13774294435");
@@ -55,6 +56,8 @@ public class AddUserServiceIT{
         Assert.assertEquals(userinfo.getSeriesNumber(), "3345");
         Assert.assertEquals(userinfo.getClientVersion(), "ios7.0");	
         
+        String userJson =  JSON.toJSONString(userinfo, true);
+        System.out.println("json" +userJson);
         
         
     }
