7c8a98160f2ae86de6f78892158561a90d4fb553
rack <373648449@qq.com>
2015-07-27 16:06:00 +0800

    test

---
+++ fotoplace-autotest/src/main/java/com/fotoplace/http/user/UserLoginIT.java
@@ -0,0 +1,17 @@
+package com.fotoplace.http.user;
+
+import com.fotoplace.http.jsons.UserServiceJSON;
+import com.pajk.test.database.DBInfo;
+
+public class UserLoginIT {
+	
+	public static final String conn =  DBInfo.getConn("fotoplaceDB");
+	public static final String serverIP = DBInfo.getConn("serverIP");
+	
+		 
+      public void getUserLogin_normal() throws Exception {
+    	
+    	//String userjson =  UserServiceJSON.
+    	
+    }
+}
---
+++ fotoplace-autotest/src/main/java/com/fotoplace/user/test/modl/UserLogin.java
@@ -0,0 +1,22 @@
+package com.fotoplace.user.test.modl;
+
+public class UserLogin {
+    
+	private String emailphone;
+	private String password;
+	
+	public String getEmailphone() {
+		return emailphone;
+	}
+	public void setEmailphone(String emailphone) {
+		this.emailphone = emailphone;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	 
+}
