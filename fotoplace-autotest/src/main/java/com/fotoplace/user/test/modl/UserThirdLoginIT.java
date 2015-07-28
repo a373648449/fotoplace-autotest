e89705f8beb82578e08ff610792e2ff459bfdeac
rack <373648449@qq.com>
2015-07-28 11:41:12 +0800

    TEST

---
+++ fotoplace-autotest/src/main/java/com/fotoplace/user/test/modl/UserThirdLoginIT.java
@@ -0,0 +1,111 @@
+package com.fotoplace.user.test.modl;
+ 
+public class UserThirdLoginIT {
+	
+	// 手机操作系统类型
+ 
+    private String osType;
+    // 第三放登录类型
+  
+    private int thirdPartType;
+    // 第三放登录Id
+ 
+    private String thirdPartId;
+    // 第三方认证
+ 
+    private String authorization;
+    // 第三方登录用户名
+ 
+    private String thirdPartName;
+    // 第三方登录头像
+  
+    private String thirdPartAvatar;
+
+ 
+    private String clintVersion;
+
+ 
+    private String seriesNumber;
+
+
+	public String getOsType() {
+		return osType;
+	}
+
+
+	public void setOsType(String osType) {
+		this.osType = osType;
+	}
+
+
+	public int getThirdPartType() {
+		return thirdPartType;
+	}
+
+
+	public void setThirdPartType(int thirdPartType) {
+		this.thirdPartType = thirdPartType;
+	}
+
+
+	public String getThirdPartId() {
+		return thirdPartId;
+	}
+
+
+	public void setThirdPartId(String thirdPartId) {
+		this.thirdPartId = thirdPartId;
+	}
+
+
+	public String getAuthorization() {
+		return authorization;
+	}
+
+
+	public void setAuthorization(String authorization) {
+		this.authorization = authorization;
+	}
+
+
+	public String getThirdPartName() {
+		return thirdPartName;
+	}
+
+
+	public void setThirdPartName(String thirdPartName) {
+		this.thirdPartName = thirdPartName;
+	}
+
+
+	public String getThirdPartAvatar() {
+		return thirdPartAvatar;
+	}
+
+
+	public void setThirdPartAvatar(String thirdPartAvatar) {
+		this.thirdPartAvatar = thirdPartAvatar;
+	}
+
+
+	public String getClintVersion() {
+		return clintVersion;
+	}
+
+
+	public void setClintVersion(String clintVersion) {
+		this.clintVersion = clintVersion;
+	}
+
+
+	public String getSeriesNumber() {
+		return seriesNumber;
+	}
+
+
+	public void setSeriesNumber(String seriesNumber) {
+		this.seriesNumber = seriesNumber;
+	}
+	
+
+}
